3a8eea3fa77facf41be455431826631ec0d2c161
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-02-17 11:31:05 -0800

    Add AOC 2015 day 1 input and solutions.

---
+++ src/2015/day_01/aoc_2015_1_1.cc
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "error: missing puzzle input file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream input(argv[1]);
+    std::string instructions;
+    std::getline(input, instructions);
+
+    int floor = 0;
+    for (const char& step : instructions) {
+        floor = (step == '(') ? (floor + 1) : (floor - 1);
+    }
+    std::cout << "Answer: " << floor << std::endl;
+
+    exit(EXIT_SUCCESS);
+}
---
+++ src/2015/day_01/aoc_2015_1_2.cc
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "error: missing puzzle input file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream input(argv[1]);
+    std::string instructions;
+    std::getline(input, instructions);
+
+    int search_pos = 0;
+    int floor = 0;
+    for (int i = 0; i < instructions.size(); ++i) {
+        floor = (instructions[i] == '(') ? (floor + 1) : (floor - 1);
+        if (floor < 0) {
+            search_pos = i + 1; /* +1 because positions are 1-indexed. */
+            break;
+        }
+    }
+    std::cout << "Answer: " << search_pos << std::endl;
+
+    exit(EXIT_SUCCESS);
+}
