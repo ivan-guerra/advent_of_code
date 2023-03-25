e53231103228b4a20e17f094eb07c46b524de1f9
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-03-25 10:45:19 -0700

    Add AOC 2017 day 1 input and solution.

---
+++ src/2017/day_01/aoc_2017_1_2.cc
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int SolveCaptcha(const std::string& captcha) {
+    int captcha_sum = 0;
+    int half_distance = captcha.size() / 2;
+    for (int i = 0; i < captcha.size(); ++i) {
+        int neighbor = (i + half_distance) % captcha.size();
+        if (captcha[i] == captcha[neighbor]) {
+            captcha_sum += static_cast<int>(captcha[i] - '0');
+        }
+    }
+    return captcha_sum;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "error: missing puzzle input file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream input(argv[1]);
+    std::string captcha;
+    std::getline(input, captcha);
+
+    std::cout << "Answer: " << SolveCaptcha(captcha) << std::endl;
+
+    exit(EXIT_SUCCESS);
+}
