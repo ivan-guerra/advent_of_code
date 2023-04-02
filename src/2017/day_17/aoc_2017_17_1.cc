402b622d52a9cd7b5dcc39a1076256abf72488c2
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-04-02 10:39:15 -0700

    Add AOC 2017 day 17 input and solution.

---
+++ src/2017/day_17/aoc_2017_17_1.cc
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int ComputeSpinlockValue(int step, int num_iter) {
+    std::vector<int> buffer;
+    buffer.push_back(0);
+
+    int curr_pos = 0;
+    for (int i = 1; i <= num_iter; ++i) {
+        for (int j = 0; j < step; ++j) {
+            curr_pos = (curr_pos + 1) % buffer.size();
+        }
+
+        curr_pos++;
+        if (buffer.size() == curr_pos) {
+            buffer.push_back(i);
+        } else {
+            buffer.insert(buffer.begin() + curr_pos, i);
+        }
+    }
+    return buffer[(curr_pos + 1) % buffer.size()];
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "error: missing puzzle input file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream input(argv[1]);
+    std::string text;
+    std::getline(input, text);
+
+    int step = std::stoi(text);
+    const int kNumIter = 2017;
+    std::cout << "Answer: " << ComputeSpinlockValue(step, kNumIter)
+              << std::endl;
+
+    exit(EXIT_SUCCESS);
+}
---
+++ src/2017/day_17/aoc_2017_17_2.cc
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int ComputeSpinlockValue(int step, int num_iter) {
+    int curr_pos = 0;
+    int value_after_zero = 0;
+    int buffer_size = 1;
+    for (int i = 1; i <= num_iter; ++i) {
+        curr_pos = (curr_pos + step) % buffer_size;
+        if (curr_pos == 0) {
+            value_after_zero = i;
+        }
+        buffer_size++;
+        curr_pos++;
+    }
+    return value_after_zero;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "error: missing puzzle input file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream input(argv[1]);
+    std::string text;
+    std::getline(input, text);
+
+    int step = std::stoi(text);
+    const int kNumIter = 50 * 1000000;
+    std::cout << "Answer: " << ComputeSpinlockValue(step, kNumIter)
+              << std::endl;
+
+    exit(EXIT_SUCCESS);
+}
