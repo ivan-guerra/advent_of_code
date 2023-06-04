c62b11e6838782b64f57846a54e75ad4926ac117
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-06-04 12:12:52 -0700

    Add AOC 2019 day 6 input and solution.

---
+++ src/2019/day_06/aoc_2019_6_1.cc
@@ -0,0 +1,55 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+
+using OrbitGraph = std::unordered_map<std::string, std::string>;
+
+void ParseOrbit(const std::string& input, OrbitGraph& orbits) {
+    std::size_t seperator = input.find(')');
+    orbits[input.substr(seperator + 1)] = input.substr(0, seperator);
+}
+
+int CountChildren(const std::string& root, const OrbitGraph& orbits) {
+    std::stack<std::string> nodes;
+    nodes.push(root);
+    int num_children = 0;
+    while (!nodes.empty()) {
+        std::string curr = nodes.top();
+        nodes.pop();
+
+        if (orbits.count(curr)) {
+            num_children++;
+            nodes.push(orbits.at(curr));
+        }
+    }
+    return num_children;
+}
+
+int ComputeOrbitChecksum(const OrbitGraph& orbits) {
+    int checksum = 0;
+    for (const auto& kv : orbits) {
+        checksum += CountChildren(kv.first, orbits);
+    }
+    return checksum;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "error: missing puzzle input file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream input(argv[1]);
+    std::string line;
+    OrbitGraph orbits;
+    while (std::getline(input, line)) {
+        ParseOrbit(line, orbits);
+    }
+
+    std::cout << "Answer: " << ComputeOrbitChecksum(orbits) << std::endl;
+
+    exit(EXIT_SUCCESS);
+}
---
+++ src/2019/day_06/aoc_2019_6_2.cc
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using OrbitGraph = std::unordered_map<std::string, std::string>;
+
+void ParseOrbit(const std::string& input, OrbitGraph& orbits) {
+    std::size_t seperator = input.find(')');
+    std::string src = input.substr(seperator + 1);
+    std::string dst = input.substr(0, seperator);
+
+    orbits[src] = dst;
+}
+
+std::vector<std::string> GetOrbitPath(const std::string& src,
+                                      const OrbitGraph& orbits) {
+    std::vector<std::string> path;
+    std::string curr = src;
+    while (orbits.at(curr) != "COM") {
+        curr = orbits.at(curr);
+        path.push_back(curr);
+    }
+    return path;
+}
+
+int FindMinTransfers(const std::string& you, const std::string& santa,
+                     const OrbitGraph& orbits) {
+    auto you_path = GetOrbitPath(you, orbits);
+    auto santa_path = GetOrbitPath(santa, orbits);
+
+    std::string intersect;
+    for (int i = 0; i < you_path.size(); ++i) {
+        for (int j = 0; j < santa_path.size(); ++j) {
+            if (you_path[i] == santa_path[j]) {
+                return i + j;
+            }
+        }
+    }
+    return -1; /* unreachable */
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "error: missing puzzle input file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream input(argv[1]);
+    std::string line;
+    OrbitGraph orbits;
+    while (std::getline(input, line)) {
+        ParseOrbit(line, orbits);
+    }
+
+    std::cout << "Answer: " << FindMinTransfers("YOU", "SAN", orbits)
+              << std::endl;
+
+    exit(EXIT_SUCCESS);
+}
